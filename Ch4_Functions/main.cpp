888e37e1430ce87ee50cdd3655e365c60ad09580
huskyhehe <zhou.he1@northeastern.edu>
2023-10-11 19:22:49 -0400

    Add ch4 functions

---
+++ Ch4_Functions/main.cpp
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include <iostream>
+
+using namespace std;
+
+/*
+* Write a program that ask for two numbers, compare them and show the maximun.
+Declare a function called max_two that compares the numbers and returns the maximun.
+*/
+int max_two() {
+	int a, b;
+	cin >> a >> b;
+	return a >= b ? a : b;
+}
+
+/*
+* Write a program that asks the user for an integer number 
+and find the sum of all natural numbers upto that number.
+*/
+int upto_sum() {
+	int n, sum = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
+int main() {
+	cout << max_two() << endl;
+	cout << upto_sum() << endl;
+	return 0;
+}
